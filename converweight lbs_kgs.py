8f608cd21350145351839df8642f9f0d4e952399
Adison <adison.fryman@gmail.com>
2022-08-17 10:02:32 -0400

    I added the time mod and cleaned up typos

--- converweight lbs_kgs.py
+++ converweight lbs_kgs.py
@@ -1,20 +1,21 @@
-
-user_weight=input('Please enter your weight in in either lbs(pounds) or kgs(killograms): ')
+import time
+user_weight = input('Please enter your weight in in either lbs(pounds) or kgs(kilograms): ')
 weight = user_weight.lower()
-if weight.find('kgs')!=-1:
-    weight_int=weight.replace('kgs','')
-    weight_in_lbs =  float(weight_int)/.453592
+if weight.find('kgs') != -1:
+    weight_int = weight.replace('kgs', '')
+    weight_in_lbs = float(weight_int)/.453592
     print(f"Your weight is ({weight_in_lbs})lbs")
-elif weight.find('lbs')!=-1:
-    weight_int=weight.replace('lbs','')
+elif weight.find('lbs') != -1:
+    weight_int = weight.replace('lbs', '')
     weight_in_kgs = float(weight_int) * .453592
     print(f"Your weight is ({weight_in_kgs})kgs")
 else:
-    weight_int = weight.replace('kgs', '')
-    weight_int = weight.replace('lbs', '')
+    weight_int = weight.replace('kgs', '') or weight.replace('lbs', '')
     weight_in_lbs = float(weight_int) / .453592
     weight_in_kgs = float(weight_int) * .453592
     print(f"Your weight is either ({weight_in_lbs})lbs or ({weight_in_kgs})kgs, but you did not include your units so I am not sure.")
 
 
+print(" Dont worry we wont tell anyone... Good bye")
 
+time.sleep(4)
