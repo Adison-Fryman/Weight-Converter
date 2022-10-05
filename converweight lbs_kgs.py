e2b80caf5621b29a8b475223a3c5cf1ca55675ec
Adison <adison.fryman@gmail.com>
2022-10-05 09:00:17 -0400

    rounded decimals in answer

--- converweight lbs_kgs.py
+++ converweight lbs_kgs.py
@@ -4,18 +4,21 @@ weight = user_weight.lower()
 if weight.find('kgs') != -1:
     weight_int = weight.replace('kgs', '')
     weight_in_lbs = float(weight_int)/.453592
-    print(f"Your weight is ({weight_in_lbs})lbs")
+    print(f"Your weight is ({round(weight_in_lbs,2)})lbs")
 elif weight.find('lbs') != -1:
     weight_int = weight.replace('lbs', '')
     weight_in_kgs = float(weight_int) * .453592
-    print(f"Your weight is ({weight_in_kgs})kgs")
+    print(f"Your weight is ({round(weight_in_kgs,2)})kgs")
 else:
     weight_int = weight.replace('kgs', '') or weight.replace('lbs', '')
     weight_in_lbs = float(weight_int) / .453592
     weight_in_kgs = float(weight_int) * .453592
-    print(f"Your weight is either ({weight_in_lbs})lbs or ({weight_in_kgs})kgs, but you did not include your units so I am not sure.")
+    print(f"Your weight is either ({round(weight_in_lbs,2)})lbs or ({round(weight_in_kgs,2)})kgs, but you did not include your units so I am not sure.")
 
 
 print(" Dont worry we wont tell anyone... Good bye")
 
-time.sleep(4)
+time.sleep(6)
+
+
+
